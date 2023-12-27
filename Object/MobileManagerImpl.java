8e89ec315dffd40cb176229d837174889b396ebb
thaisonhp <106387952+thaisonhp@users.noreply.github.com>
2023-12-27 11:38:42 +0700

    Add files via upload

---
+++ Object/MobileManagerImpl.java
@@ -0,0 +1,85 @@
+package Object;
+
+import java.util.ArrayList;
+
+public class MobileManagerImpl implements MobileManager {
+	private ArrayList<Mobile> ListMobile ; 
+	
+	public MobileManagerImpl() {
+		this.ListMobile = new ArrayList<>() ; 
+	}
+	
+	public MobileManagerImpl(ArrayList<Mobile> ListMobile) {
+		this.ListMobile = ListMobile ;
+	}
+	
+	@Override
+	public boolean addMobile(Mobile m)  throws NullPointerException {
+		// TODO Auto-generated method stub
+		boolean find = false ; 
+		try {
+			for(Mobile i : this.ListMobile) {
+				if(i.getProduct_id() == m.getProduct_id()) {
+					find = true ;
+					break ;
+				}
+			}
+			if(find) return false; 
+			else {
+				this.ListMobile.add(m);
+				return true ;
+			}
+		}catch(NullPointerException ex) {
+			ex.getStackTrace();
+			return false ;
+		}
+	}
+
+	@Override
+	public boolean delMobile(Mobile m) throws NullPointerException {
+		boolean result = false ;
+		try {
+			for(Mobile i :this.ListMobile) {
+				if(i.getProduct_id() == m.getProduct_id()) {
+					this.ListMobile.remove(i);
+					result = true; 
+					break; 
+				}
+			}
+			return result ;
+		}catch( NullPointerException ex) {
+			ex.getStackTrace();
+			return false;
+		}
+	}
+
+	public ArrayList<Mobile> searchMobile(String name)  throws NullPointerException{
+		ArrayList<Mobile> results = new  ArrayList<>() ; 
+		try { 
+			this.ListMobile.forEach(go->{
+				if(go.getProduct_name().contains(name)) {
+					results.add(go);
+				}
+			});
+			return results;
+		}catch( NullPointerException ex) {
+			ex.getStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public double getTotalStorage() throws NullPointerException{
+		double result = 0  ;
+		try {
+			// TODO Auto-generated method stub
+			for(Mobile i : this.ListMobile) {
+				result += i.getTotalPrice() ;
+			}
+			return result ;
+		}catch( NullPointerException ex) {
+			ex.getStackTrace();
+			return 0.0;
+		}
+	}
+}
---
+++ Object/Test.java
@@ -0,0 +1,50 @@
+package Object;
+
+import java.util.ArrayList;
+
+public class Test {
+	private static ArrayList<Mobile> ListMobile ; 
+	
+	public void generateList(int n) {
+		Test.ListMobile = new ArrayList<>(); 
+		String[] name = {"ip11","ss22","oppo21","xiaomi03","ip12prm","ip12","ipx"};
+		String[] manufacturer = {"ip","ss","xiaomi","lg"};
+ 		int cnt ; 
+ 		for(int i = 1 ; i <= n ; i++ )
+		{
+ 			Mobile m = new Mobile(); 
+			m.setProduct_id(i);
+			cnt = (int)(Math.random()*(name.length));
+			m.setProduct_name(name[cnt]);
+			m.setProduct_price((int)(Math.random()*1000));
+			m.setProduct_total((int)(Math.random()*100));
+			cnt = (int)(Math.random()*manufacturer.length) ; 
+			m.setMobile_manufacturer(manufacturer[cnt]);
+			m.setMobile_ram(Math.random()*10);
+			ListMobile.add(m);
+		}
+ 		
+	}
+	public void printList() {
+		Test.ListMobile.forEach(go->{
+			System.out.println(go);
+		});
+	}
+	public void printList(ArrayList<Mobile> list) {
+		list.forEach(go->{
+			System.out.println(go);
+		});
+	}
+	public static void main(String[] args) {
+		Test o1 = new Test() ; 
+		o1.generateList(5);
+		o1.printList();
+		MobileManagerImpl manager = new MobileManagerImpl(ListMobile);
+		manager.addMobile(new Mobile(6,"ip6",1000,10,"ip",2));
+		System.out.println("Ket qua tim kiem:");
+		ArrayList<Mobile> results = manager.searchMobile("ip6");
+		o1.printList(results);
+		manager.delMobile(new Mobile(6,"ip6",1000,10,"ip",2));
+		System.out.print(manager.getTotalStorage());
+	}
+}
